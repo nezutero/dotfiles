8617032feb9b61cbffe7f3b5f43805c7952ad4ee
plastiey <btwdesu@gmail.com>
2024-04-13 00:22:58 +0300

    feat: update install script

--- install.sh
+++ install.sh
@@ -1,62 +1,50 @@
-# install zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#!/bin/bash
 
+# update package repos
+sudo pacman -Syu
+
+# packages
+sudo pacman -S --noconfirm \
+    firefox git neovim zathura dunst btop waybar swaybg alacritty ranger rofi \
+    telegram-desktop wl-clipboard grim slurp npm pnpm yarn gimp qt5ct lxappearance \
+    neofetch zsh materia-gtk-theme nvidia nvidia-utils pavucontrol zip unzip tree \
+    xf86-video-intel networkmanager lib32-nvidia-utils feh noto-fonts-cjk zig \
+    python nodejs ttf-dejavu noto-fonts-emoji zathura-pdf-mupdf fzf docker docker-compose \
+    alsa-utils
+
+# yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+# dir structure
+mkdir -p ~/dev/mantain
+mkdir -p ~/docs/{books,watashi}
+mkdir -p ~/pics/{walls,screenshots,avatars,logos}
+mkdir -p ~/vids/screencaptures
+mkdir -p ~/.local/bin
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# install rust
+# rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# install my fav nerd font: cascadia code
+# cascadia code
 yay -S ttf-cascadia-code-nerd
 
-# clone nvim config
-git clone https://github.com/plastiey/nvim.git ~/.config/neovim
+# nvim config
+git clone https://github.com/kenjitheman/nvim.git ~/.config/neovim
 
-# install packer.nvim
+# packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-# cd into dotfiles directory
-cd ~/dotfiles
-
-# create .config directories if they don't exist
-mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/btop
-mkdir -p ~/.config/dunst
-mkdir -p ~/.config/hypr
-mkdir -p ~/.config/neofetch
-mkdir -p ~/.config/ranger
-mkdir -p ~/.config/rofi
-mkdir -p ~/.config/waybar
-mkdir -p ~/.config/zathura
-mkdir -p ~/.config/nvim
-
-# create .local/bin directories if they don't exist
-mkdir -p ~/.local/bin
-
-# create symlinks for .config directory
-ln -sf ~/.dotfiles/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
-ln -sf ~/.dotfiles/.config/btop/btop.conf ~/.config/btop/btop.conf
-ln -sf ~/.dotfiles/.config/dunst/dunstrc ~/.config/dunst/dunstrc
-ln -sf ~/.dotfiles/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -sf ~/.dotfiles/.config/neofetch/config.conf ~/.config/neofetch/config.conf
-ln -sf ~/.dotfiles/.config/ranger/rc.conf ~/.config/ranger/rc.conf
-ln -sf ~/.dotfiles/.config/rofi/config.rasi ~/.config/rofi/config.rasi
-ln -sf ~/.dotfiles/.config/waybar/config ~/.config/waybar/config
-ln -sf ~/.dotfiles/.config/waybar/style.css ~/.config/waybar/style.css
-ln -sf ~/.dotfiles/.config/zathura/zathurarc ~/.config/zathura/zathurarc
-
-# create symlinks for other dotfiles
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/install.sh ~/install.sh
-ln -sf ~/.dotfiles/.local/bin/chman-commands ~/.local/bin/chman-commands
-ln -sf ~/.dotfiles/.local/bin/chman-langs ~/.local/bin/chman-langs
-ln -sf ~/.dotfiles/.local/bin/chman.sh ~/.local/bin/chman.sh
-ln -sf ~/.dotfiles/.local/bin/fzfman.sh ~/.local/bin/fzfman.sh
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
-
-# create symlinks for root -> home
-sudo mkdir -p /root/.config
-sudo ln -sf ~/.dotfiles/.zshrc /root/.zshrc
-sudo ln -sf ~/.oh-my-zsh /root/.oh-my-zsh
-sudo ln -sf ~/.config/nvim /root/.config/nvim
+# spotify
+yay -S spotify
