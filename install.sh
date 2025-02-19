1261e57c6eb8e324f150d057770b31f1fcf863c3
nezutero <nezutero@gmail.com>
2025-02-19 13:15:39 +0100

    feat: update zathura config

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo pacman -S --noconfirm \
     fastfetch zsh materia-gtk-theme pavucontrol zip unzip tree obs-studio audacity steam \
     imv mpv noto-fonts-cjk zig python nodejs ttf-dejavu noto-fonts-emoji kdenlive sof-firmware \
     zathura-pdf-mupdf fzf docker docker-compose alsa-utils dnsutils distrobox bluez bluez-utils cheese \
-    wlogout vesktop spotify video-downloader ncdu
+    wlogout vesktop spotify video-downloader ncdu noto-fonts noto-fonts-cjk noto-fonts-emoji
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
