1457d133dc715fd01e744af770f8592b0a750436
nezutero <nezutero@pm.me>
2025-03-05 20:49:02 +0100

    feat: add zsh-vi-mode plugin)

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}plugins/zsh-vi-mode
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
