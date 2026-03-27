d02ffc0908f323e81a39ca8a9341ec22b9e638f9
nezutero <me@nezutero.dev>
2026-03-27 20:30:59 +0100

    feat: add more themes, update look and feel

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -138,15 +138,33 @@ require("lazy").setup({
         config = function()
             require("zen-mode").setup({
                 window = {
-                    backdrop = 0.10,
-                    width = 110,
+                    backdrop = 1,
+                    width = 90,
+                    height = 1,
+                    padding = {
+                        left = 0,
+                        right = 0,
+                    },
                     options = {
                         number = false,
                         relativenumber = false,
+                        signcolumn = "no",
+                        foldcolumn = "0",
+                        list = false,
                     }
                 },
+                plugins = {
+                    twilight = { enabled = false },
+                    gitsigns = { enabled = false },
+                    tmux = { enabled = false },
+                    kitty = { enabled = false },
+                    alacritty = {
+                        enabled = true,
+                        font = "16",
+                    },
+                },
                 on_open = function()
-                    vim.api.nvim_set_hl(0, 'ZenBg', { bg = '#141414' })
+                    vim.api.nvim_set_hl(0, 'ZenBg', { bg = 'NONE' })
                 end,
             })
         end
