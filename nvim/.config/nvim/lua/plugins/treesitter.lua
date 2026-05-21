02de9d19b84c20d5d1337725d7203f0a67f8d41d
nezutero <me@nezutero.dev>
2026-05-21 15:23:35 +0200

    fix: Lua chunk not found

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,7 @@
-require ("nvim-treesitter.configs").setup {
-    -- A list of parser names, or "all"
+local ok, configs = pcall(require, "nvim-treesitter.configs")
+if not ok then return end
+
+configs.setup {
     ensure_installed = {
         "vimdoc",
         "javascript",
@@ -24,26 +26,12 @@ require ("nvim-treesitter.configs").setup {
         "yaml",
         "markdown",
         "sql",
-        "json",
-        "yaml",
         "toml",
     },
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
-
     highlight = {
-        -- `false` will disable the whole extension
         enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
     },
 }
