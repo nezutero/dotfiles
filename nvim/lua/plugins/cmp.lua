b1e77cac215783b41d8c0681e729186b1f97e3c2
nezutero <me@nezutero.dev>
2026-06-25 15:52:57 +0200

    chore: merge nvim into dotfiles

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,73 @@
+return {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "L3MON4D3/LuaSnip",
+    },
+
+    config = function()
+        local cmp = require("cmp")
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+                -- open completion manually
+                ["<C-Space>"] = cmp.mapping.complete(),
+
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+                ["<C-n>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
+                ["<C-p>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            }),
+
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+            }, {
+                { name = "buffer" },
+                { name = "path" },
+            }),
+        })
+
+        vim.diagnostic.config({
+            update_in_insert = false,
+            virtual_text = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+    end,
+}
---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,87 @@
+return {
+--    "neovim/nvim-lspconfig",
+--
+--    dependencies = {
+--        "williamboman/mason.nvim",
+--        "williamboman/mason-lspconfig.nvim",
+--        "hrsh7th/cmp-nvim-lsp",
+--        "j-hui/fidget.nvim",
+--    },
+--
+--    config = function()
+--        vim.env._JAVA_OPTIONS =
+--        "-Djdk.xml.totalEntitySizeLimit=0 -Djdk.xml.entityExpansionLimit=0"
+--
+--        local capabilities =
+--            require("cmp_nvim_lsp").default_capabilities()
+--
+--        require("mason").setup()
+--
+--        require("mason-lspconfig").setup({
+--            ensure_installed = {
+--                "bashls",
+--                "clangd",
+--                "eslint",
+--                "gopls",
+--                "html",
+--                "ts_ls",
+--                "marksman",
+--                "rust_analyzer",
+--                "lua_ls",
+--                "zls",
+--                "cssls",
+--                "nil_ls",
+--            },
+--        })
+--
+--        local servers = {
+--            bashls = {},
+--            clangd = {
+--                cmd = { vim.fn.exepath("clangd") },
+--            },
+--            eslint = {},
+--            gopls = {},
+--            html = {},
+--            ts_ls = {},
+--            marksman = {},
+--            rust_analyzer = {},
+--            zls = {},
+--            cssls = {},
+--            nil_ls = {},
+--
+--            lua_ls = {
+--                settings = {
+--                    Lua = {
+--                        diagnostics = {
+--                            globals = { "vim" },
+--                        },
+--                    },
+--                },
+--            },
+--        }
+--
+--        for server, opts in pairs(servers) do
+--            opts.capabilities = capabilities
+--            vim.lsp.config(server, opts)
+--            vim.lsp.enable(server)
+--        end
+--
+--        vim.api.nvim_create_autocmd("LspAttach", {
+--            callback = function(ev)
+--                local opts = { buffer = ev.buf, silent = true }
+--
+--                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+--                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+--                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+--                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+--                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+--                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+--                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+--                vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+--                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+--                vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+--                vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
+--            end,
+--        })
+--    end,
+}
