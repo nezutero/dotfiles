700dc80ed1b7e248969df302cdcd6aaa951ce172
nezutero <me@nezutero.dev>
2026-09-07 08:40:25 +0200

    feat: update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -e  # Exit on error
 
 echo "Starting system setup..."
@@ -8,19 +9,17 @@ sudo pacman -Syu --noconfirm
 
 echo "Installing packages..."
 sudo pacman -S --noconfirm \
-    zathura dunst btop waybar hypridle hyprlock hyprpaper hyprsunset alacritty \
-    wl-clipboard grim slurp npm pnpm yarn gimp qt5ct nwg-look wev tmux brightnessctl cliphist \
-    qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat thunar ripgrep fd glow \
-    fastfetch zsh materia-gtk-theme pavucontrol zip unzip tree obs-studio audacity telegram-desktop \
-    imv mpv noto-fonts-cjk zig python nodejs ttf-dejavu noto-fonts-emoji sof-firmware \
-    docker docker-compose alsa-utils dnsutils distrobox bluez bluez-utils cheese \
-    ncdu noto-fonts stow rofi shotwell \
-    nasm speedcrunch pacman-contrib ttf-fira-code thunderbird \
-    neovim yazi imagemagick chafa \
-    gdb bat eza fzf
+    zathura dunst btop waybar hypridle hyprlock hyprpaper hyprsunset wl-clipboard \
+    grim slurp gimp qt5ct wev tmux brightnessctl cliphist dnsmasq vde2 ripgrep fd \
+    fastfetch zsh pavucontrol zip unzip tree obs-studio audacity telegram-desktop \
+    imv mpv noto-fonts-cjk python nodejs ttf-dejavu noto-fonts-emoji docker gdb fzf \
+    docker-compose alsa-utils dnsutils distrobox cheese ncdu noto-fonts stow rofi \
+    nasm speedcrunch pacman-contrib ttf-fira-code thunderbird neovim yazi imagemagick
+    signal-desktop foot git curl
 
 echo "Installing yay..."
 if ! command -v yay &> /dev/null; then
+    sudo pacman -S --needed --noconfirm base-devel
     git clone https://aur.archlinux.org/yay.git /tmp/yay
     cd /tmp/yay
     makepkg -si --noconfirm
@@ -33,19 +32,15 @@ fi
 echo "Installing AUR packages..."
 yay -S --noconfirm \
     ttf-jetbrains-mono-nerd \
-    gruvbox-material-gtk-theme-git \
-    gruvbox-plus-icon-theme \
-    anki \
-    resvg \
-    rofi-bluetooth-git \
-    cheese
+    kanagawa-gtk-theme-git \
+    kanagawa-icon-theme-git \
+    anki resvg rofi-bluetooth-git \
 
 echo "Creating directories..."
 mkdir -p ~/projs
-mkdir -p ~/docs/{books}
+mkdir -p ~/docs
 mkdir -p ~/pics/{walls,screenshots,webcam,backs,pfps}
-mkdir -p ~/videos/{OBS,webcam}
-mkdir -p ~/.config
+mkdir -p ~/videos/OBS
 
 echo "Installing Oh My Zsh..."
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
@@ -55,6 +50,7 @@ else
 fi
 
 echo "Installing Zsh plugins..."
+mkdir -p ~/.oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" 2>/dev/null || echo "zsh-autosuggestions already exists"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" 2>/dev/null || echo "zsh-syntax-highlighting already exists"
 
@@ -94,6 +90,12 @@ else
     echo "WARNING: ~/dotfiles directory not found! Skipping stow..."
 fi
 
+echo "Configuring GTK theme, icons, and fonts..."
+gsettings set org.gnome.desktop.interface gtk-theme 'Kanagawa-Yellow-Dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Kanagawa'
+gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono Nerd Font 14'
+echo "GTK settings applied!"
+
 echo "Enabling services..."
 sudo systemctl enable --now docker
 sudo systemctl enable --now bluetooth
@@ -102,12 +104,46 @@ sudo systemctl enable --now tlp
 echo "Adding user to docker group..."
 sudo usermod -aG docker "$USER"
 
+echo "Sourcing tmux..."
+tmux source-file ~/.config/tmux.conf
+
+echo "Configuring locale..."
+sudo sed -i 's/^#\(en_GB.UTF-8 UTF-8\)/\1/' /etc/locale.gen
+sudo locale-gen
+sudo localectl set-locale LANG=en_GB.UTF-8
+
+echo "Configuring SSH..."
+mkdir -p "$HOME/.ssh"
+chmod 700 "$HOME/.ssh"
+
+if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
+    ssh-keygen -t ed25519 -C "me@nezutero.dev" -f "$HOME/.ssh/id_ed25519" -N ""
+else
+    echo "SSH key already exists, skipping..."
+fi
+
+cat > "$HOME/.ssh/config" <<EOF
+Host github.com
+    HostName github.com
+    User git
+    IdentityFile ~/.ssh/id_ed25519
+    IdentitiesOnly yes
+EOF
+
+chmod 600 "$HOME/.ssh/config"
+
+echo "Setting Zsh as default shell..."
+if [ "$SHELL" != "$(which zsh)" ]; then
+    chsh -s "$(which zsh)"
+else
+    echo "Zsh is already the default shell."
+fi
+
 echo "============================================"
 echo "Installation complete!"
 echo "============================================"
 echo "TODO:"
-echo "1. Log out and back in for docker group to take effect"
-echo "2. Run 'tmux' and press prefix + I to install tmux plugins"
-echo "3. Open nvim and run :Lazy and :Mason"
-echo "4. Change default shell: chsh -s /bin/zsh"
+echo "- Log out and back in for docker group to take effect"
+echo "- Open nvim and run :Lazy and :Mason"
+echo "- Add your SSH key: https://github.com/settings/keys"
 echo "============================================"
